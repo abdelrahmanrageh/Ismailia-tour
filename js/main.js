8c262ecb72b825727aba77c32f01bd11e97d58f8
abdorageh <abdelrahmanrageh@gmail.com>
2024-02-25 17:36:39 +0200

    modifing year

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//scroll to top button
 let play = document.getElementById("top");
 
 window.onscroll = function() {
@@ -18,9 +20,10 @@ const navLinks = document.querySelectorAll(".slider-nav a");
 
 slider.addEventListener("scroll", function () {
 
-    const activeSlide = Math.round(slider.scrollLeft / slider.clientWidth);
     // Calculate the index of the active slide based on the slider's scroll position.
-
+    const activeSlide = Math.round(slider.scrollLeft / slider.clientWidth);
+    
+    // Checking if index of slide equals activeSlide
     navLinks.forEach((link, index) => {
         if (index === activeSlide) {
             link.classList.add("active");
@@ -29,7 +32,6 @@ slider.addEventListener("scroll", function () {
             link.classList.remove("active");
         }
     });
-    // Checking if index of slide equals activeSlide
 
 });
 
@@ -37,20 +39,7 @@ var textWrapper = document.querySelector('.ml2');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml2 .letter',
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 950,
-    delay: (el, i) => 70*i
-  }).add({
-    targets: '.ml2',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+//year
+let year = document.querySelector(".year");
+year.innerHTML = new Date().getFullYear();
 
